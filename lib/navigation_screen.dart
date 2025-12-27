244de6d99720f042e20897c256a0c68a0fb86087
CyberJunior18 <scihammo@gmail.com>
2025-12-27 21:52:29 +0200

    appbar done

--- lib/navigation_screen.dart
+++ lib/navigation_screen.dart
@@ -1,12 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'learning_screen.dart';
 
 class NavigationScreen extends StatelessWidget {
+  const NavigationScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: LearningScreen(), // opens HomeScreen by default
+      backgroundColor: const Color(0xFF1F1F39),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF3D5CFF),
+        toolbarHeight: 100,
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          color: Colors.white,
+          onPressed: () {},
+        ),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Hi, Mostafa',
+              style: GoogleFonts.poppins(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              "Let's start learning",
+              style: GoogleFonts.poppins(
+                fontSize: 16,
+                color: Colors.white70,
+              ),
+            ),
+          ],
+        ),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://i.pravatar.cc/150?img=3'),
+            ),
+          ),
+        ],
+      ),
+      body: const LearningScreen(),
     );
   }
 }
